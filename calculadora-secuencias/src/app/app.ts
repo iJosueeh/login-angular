c7a86d629ea764c6d9a8d7290af4acdf965f5a1f
kath144 <mscatalino2@gmail.com>
2025-09-20 19:46:24 -0500

    backup antes de limpiar dependencias y duplicados

--- calculadora-secuencias/src/app/app.ts
+++ calculadora-secuencias/src/app/app.ts
@@ -1,11 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    Header 
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('calculadora-secuencias');
